010e828712b55e9c9eb96b7c088dbe3907b975a4
Thierry Caron <thierry.caron93@gmail.com>
2018-03-20 19:28:55 +0100

    fix bug chambre

--- src/Controller/ChambreController.php
+++ src/Controller/ChambreController.php
@@ -5,28 +5,37 @@ use App\Entity\Chambre;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+* @Route("/chambre")
+*/
 class ChambreController extends Controller
 {
     /**
-     * @Route("/chambre")
+     * @Route("/")
      */
     public function index()
     {
     	$repository = $this->getDoctrine()->getRepository(Chambre::class);
         $chambres = $repository->findAll();
         return $this->render('chambre/index.html.twig', [
-            'chambres' => $chambres
+            'chambres' => $chambres,
+            
         ]);
     }
     /**
-    *@Route("/chambre/preview")
+    *@Route("/{id}")
     */
-    public function preview(){
-        $repository = $this->getDoctrine()->getRepository(Chambre::class);
-        $chambres = $repository->findAll();
-
-        return $this->render('chambre/preview.html.twig', [
-            'chambres' => $chambres]);}
+    public function preview($id){
+        
+        $em = $this->getDoctrine()->getManager();
+        $chambre = $em->find(Chambre::class, $id);
+        
+        return $this->render('chambre/preview.html.twig', 
+                [
+                   'chambre' => $chambre
+                ]);
+        
+    }
 
 }
 
